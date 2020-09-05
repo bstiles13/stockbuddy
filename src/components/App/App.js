3649b7cfd14db927c6d4d70a17751a107c350860
Brandon Stiles <bstiles@merkleinc.com>
2020-09-05 15:56:34 -0400

    adds theme toggle

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar } from '../Navbar/Navbar';
 import { Trending } from '../Trending/Trending';
 import { Watchlist } from '../Watchlist/Watchlist';
 
+import '../../styles/themes.scss';
 import './App.scss';
 
 const App = () => {
+  useEffect(() => { // mount
+    document.body.classList.add('dark');
+  }, []);
+
   return (
     <div className='app'>
       <Navbar />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './Navbar.scss';
 
 export const Navbar = () => {
+  const toggleTheme = theme => {
+    document.body.classList.remove('light', 'dark');
+    document.body.classList.add(theme);
+  };
+
   return (
     <div className='navbar'>
-      Navbar
+      <div>Navbar</div>
+      <div>
+        <span className='theme-toggle' onClick={() => toggleTheme('light')}>Light</span>/
+        <span className='theme-toggle' onClick={() => toggleTheme('dark')}>Dark</span>
+      </div>
     </div>
   );
 };
